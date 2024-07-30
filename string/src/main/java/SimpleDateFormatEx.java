5ad0c92d611ea70ef769f28a8253267e6a80682a
kotohiko <tachibana.kotohiko@gmail.com>
2024-07-30 17:52:39 +0800

    添加string模块

---
+++ string/src/main/java/SimpleDateFormatEx.java
@@ -0,0 +1,18 @@
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ * @author Jacob Suen
+ * @apiNote 2024 年就职于金蝶期间，某天编码遇到了一个不可思议的业务场景卡壳。拿到一个{@link java.util.Date}对象，如何转为国内流行的
+ * {@code 20**-**-**}格式？试了好多种方案都没达到满意效果，后来问了 AI 模型才拿到了达到预期效果的解决方案。那便是通过
+ * {@link java.text.SimpleDateFormat}来达成需求效果。
+ * @since 14:02 Jul 30, 2024
+ */
+public class SimpleDateFormatEx {
+    public static void main(String[] args) {
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        Date date = new Date();
+        System.out.println("Construct a Date object directly is like: [" + date + "]");
+        System.out.println("After formatted by SimpleDateFormat is like: [" + sdf.format(date) + "]");
+    }
+}
--- web/src/main/java/com/jacob/json/formatter/google/api/GsonJsonFormatter.java
+++ web/src/main/java/com/jacob/json/formatter/google/api/GsonJsonFormatter.java
@@ -3,6 +3,9 @@ package com.jacob.json.formatter.google.api;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
+/**
+ * 调用 Gson 接口
+ */
 public class GsonJsonFormatter {
     public static String formatJsonByGson(String unformattedJson) {
         Gson gson = new GsonBuilder().setPrettyPrinting().create();
