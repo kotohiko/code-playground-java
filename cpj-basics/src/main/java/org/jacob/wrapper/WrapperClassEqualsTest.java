046ab6dbef1d72e19b58700ed731e1741871bcb3
JacobSuen <tachibana.kotohiko@gmail.com>
2024-10-13 15:25:36 +0800

    包装类的讨论

---
+++ cpj-basics/src/main/java/org/jacob/wrapper/WrapperClassEqualsTest.java
@@ -0,0 +1,13 @@
+package org.jacob.wrapper;
+
+/**
+ * @author Kotohiko
+ * @since 15:12 Oct 13, 2024
+ */
+public class WrapperClassEqualsTest {
+    public static void main(String[] args) {
+        Integer a = 666;
+        Integer b = 666;
+        System.out.println(a == b);
+    }
+}
